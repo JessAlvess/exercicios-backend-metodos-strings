eb0cac92955178b93ec2ab60e41724a49cca8421
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:07:05 -0300

    ex4

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,18 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+identificador = identificador.length === 6 ? identificador : identificador.padStart(6, '0')
+
+let nomeFormatado = nome.split(' ')
+let isUpperCase = str => str === str.toUpperCase() 
+for (let i = 0; i < nomeFormatado.length; i++) {
+    const string = nomeFormatado[i] 
+    nomeFormatado[i] = isUpperCase(string[0]) ? nomeFormatado[i] : string[0].toUpperCase() + nomeFormatado[i].slice(1)
+}
+
+
+
+console.log(nomeFormatado.join(' '));
+console.log(identificador);
+console.log(email.trim());
