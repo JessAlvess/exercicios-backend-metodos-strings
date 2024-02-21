09067696521093063d96b154e3727fba11593118
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:52:42 -0300

    ex9

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,5 @@
 const nome = 'Guido Cerqueira';
+let str = nome.split(' ')
+str = str.join('').toLowerCase()
+
+str.length > 13 ? console.log(`@${str.slice(0,12)}`) : console.log(`@${str.slice(0)}`)
