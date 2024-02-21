4b474a9e953e9dffc6c3c98d777e7185ff3dbc8a
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:19:11 -0300

    ex6

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,5 @@
-const celular = 7199918888;
+// const celular = 7199918888;
+const celular = 99918888;
+
+const celularStr = String(celular)
+celularStr.length === 10 ? console.log(`(${celularStr.slice(0,2)}) ${celularStr.charAt(2)} ${celularStr.slice(2,6)}-${celularStr.slice(6,10)}`) : console.log(`${celularStr.charAt(0)} ${celularStr.slice(0,4)}-${celularStr.slice(4,8)}`)
