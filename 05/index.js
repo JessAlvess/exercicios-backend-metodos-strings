43cc832f865a47e47736c3fb20840dc9500bec07
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:11:38 -0300

    ex5

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,5 @@
 const numeroCartao = '1111222233334444';
+const primeiroTermo = numeroCartao.slice(0, 4)
+const ultimoTermo = numeroCartao.slice(-4)
+const maskedNumber = `${primeiroTermo + ultimoTermo.padStart(12, '*')}`
+console.log(maskedNumber);
