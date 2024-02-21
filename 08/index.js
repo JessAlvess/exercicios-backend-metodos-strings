47e077a2426dc8fd716138c2cb957e931c95d635
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:46:42 -0300

    ex8

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,7 @@
 const cpf = "011.022.033-44";
+
+let cpfArr = cpf.split('.')
+cpfArr[cpfArr.length - 1] = cpfArr[cpfArr.length - 1].split('-')
+let cpfOut = cpfArr.join('') 
+cpfArr = cpfOut.split(',')
+console.log(cpfArr.join(''));
