869219827e9c842696c6d2f0c70a1ef7e7993f2b
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 19:47:17 -0300

    ex2

--- 02/index.js
+++ 02/index.js
@@ -1,2 +1,23 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678000199";
+
+function cpfTransform(string) {
+    const primeiroTermo = string.slice(0,3)
+    const segundoTermo = string.slice(3,6)
+    const terceiroTermo = string.slice(6,9)
+    const quartoTermo = string.slice(9)
+    return `${primeiroTermo}.${segundoTermo}.${terceiroTermo}-${quartoTermo}`
+}
+
+function cnpjTransform(string) {
+    const primeiroTermo = string.slice(0,2)
+    const segundoTermo = string.slice(2,5)
+    const terceiroTermo = string.slice(5,8)
+    const quartoTermo = string.slice(8,12)
+    const quintoTermo = string.slice(12)
+    return `${primeiroTermo}.${segundoTermo}.${terceiroTermo}/${quartoTermo}-${quintoTermo}`
+}
+
+cpf.length === 11 ? console.log(cpfTransform(cpf)): console.log('CPF Inválido');
+
+cnpj.length === 14 ? console.log(cnpjTransform(cnpj)): console.log('CNPJ Inválido');
