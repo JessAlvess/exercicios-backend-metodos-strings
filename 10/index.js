c2aadba11287aa354e00d3618faf0b7f7bf9a7be
Jessé Alves <jessealvespr@outlook.com>
2024-02-21 20:56:20 -0300

    ex10

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,7 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.jpeg';
+
+let indexPoint = nomeArquivo.indexOf('.')
+nomeArquivo.slice(indexPoint + 1) === 'jpg' || nomeArquivo.slice(indexPoint + 1) === 'jpeg' ||
+nomeArquivo.slice(indexPoint + 1) === 'gif' || nomeArquivo.slice(indexPoint + 1) === 'png' ?
+console.log('Arquivo válido') :
+console.log('Arquivo inválido')
